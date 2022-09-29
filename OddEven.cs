d1f35b2ac4d645e837ba4f99ffc722b5da79aa8c
YiSeungWoo-318 <64455707+YiSeungWoo-318@users.noreply.github.com>
2022-09-29 18:58:36 +0900

    Add files via upload

---
+++ OddEven.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+public class Solution
+{
+    public string solution(int num)
+    {   
+        int a = num;
+        int b = 2;
+        double remain = a % b;
+        if (remain == 0)
+            return "Even";
+        else
+            return "Odd";
+
+    }
+}
